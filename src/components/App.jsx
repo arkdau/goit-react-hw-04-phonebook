2371a96e6e352eaa10ffa4fa6926f34dbe387489
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-12-13 01:03:37 +0100

    fix: del useRef

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
